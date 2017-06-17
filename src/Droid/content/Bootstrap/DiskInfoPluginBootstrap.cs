5e07fabbc79a8296c7aa4fc3cb9c5a8f02441686
Denis Gordin <gordin.dan@gmail.com>
2017-06-17 17:08:01 +0600

    #

---
+++ src/Droid/content/Bootstrap/DiskInfoPluginBootstrap.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using MvvmCross.Platform.Plugins;
+
+namespace g0rdan.MvvmCross.Plugin.DiskInfo.Droid
+{
+	public class DiskInfoPluginBootstrap : MvxPluginBootstrapAction<g0rdan.MvvmCross.Plugin.DiskInfo.PluginLoader>
+	{
+	}
+}
